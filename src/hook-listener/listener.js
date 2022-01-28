a5f09e7cf9a09b16e5893c1200f56779322bf209
Oliver Sutton <osutton2010@gmail.com>
2022-01-28 19:53:59 +0000

    Changed env to be port

--- src/hook-listener/listener.js
+++ src/hook-listener/listener.js
@@ -16,8 +16,8 @@ module.exports = class Listener {
       this.discordIntegration.processReq(req);
     });
 
-    this.app.listen(process.env.WEBHOOK_PORT, () => {
-      console.log(`🚀 Server running on port ${process.env.WEBHOOK_PORT}`)
+    this.app.listen(process.env.PORT, () => {
+      console.log(`🚀 Server running on port ${process.env.PORT}`)
     });
   }
 }
